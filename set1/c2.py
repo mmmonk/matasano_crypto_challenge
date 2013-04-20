5dd2443253c120ccbac1a3d4aa07936579e9efdf
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-04-20 22:06:40 +0200

    set1 - init :)

---
+++ set1/c2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def xorss(s1,s2):
+
+  ml = len(s2)
+  if len(s1) > len(s2):
+    ml = len(s1)
+
+  return "".join([chr(ord(s1[i])^ord(s2[i])) for i in range(ml)])
+
+
+print xorss("1c0111001f010100061a024b53535009181c".decode('hex'),"686974207468652062756c6c277320657965".decode('hex')).encode('hex')
---
+++ set1/c3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import string
+
+ct = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".decode('hex')
+
+for x in range(256):
+  mesg = []
+  for i in range(len(ct)):
+    mesg.append(chr(x^ord(ct[i])))
+    if not mesg[-1] in string.printable:
+      break
+    if i + 1 == len(ct):
+      print str(x)+" "+"".join(mesg)
+
