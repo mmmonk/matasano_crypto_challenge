dddc6a509f2ab2c4e7147bee1ba8b06546e4291f
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-04-22 17:34:33 +0200

    set1/c3.py - more work done

--- set1/c3.py
+++ set1/c3.py
@@ -4,6 +4,7 @@ import string
 
 ct = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".decode('hex')
 
+
 for x in range(256):
   mesg = []
   for i in range(len(ct)):
@@ -12,4 +13,8 @@ for x in range(256):
       break
     if i + 1 == len(ct):
       print str(x)+" "+"".join(mesg)
+      chars = dict()
+      for c in mesg:
+        chars[c] = chars.get(c,0) + 1
+      print chars
 
