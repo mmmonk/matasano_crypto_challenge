8b71a6795c702ba379f1b344c771350c6461df87
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-04-24 14:49:44 +0200

    c8.py done

--- set1/c8.py
+++ set1/c8.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import urllib
-import base64
 
-ct = base64.b64decode(urllib.URLopener().open("https://gist.github.com/tqbf/3132928/raw/6f74d4131d02dee3dd0766bd99a6b46c965491cc/gistfile1.txt").read())
+def findecb(cte,blksize=16):
+  ct = cte.decode('hex')
+  a = dict()
+  for i in range(0,len(ct),blksize):
+    c = ct[i:i+blksize]
+    a[c] = a.get(c,0) + 1
+    if a[c] > 1:
+      print cte
+      break
+
+for l in urllib.URLopener().open("https://gist.github.com/tqbf/3132928/raw/6f74d4131d02dee3dd0766bd99a6b46c965491cc/gistfile1.txt").readlines():
+  findecb(l.rstrip())
