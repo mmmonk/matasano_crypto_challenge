54b597a02eb60428f3f2df6de24ff7a4c1e4a276
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-04-25 17:40:23 +0200

    ok, I think I'm done with set1

--- set1/c2.py
+++ set1/c2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-def xorss(s1,s2): # xor for strings
+def xorss(s1,s2): # xor for strings - non repetitive
   if len(s1) > len(s2):
-    return "".join([chr(ord(s1[i])^ord(s2[i])) for i in range(len(s1))])
-  return "".join([chr(ord(s1[i])^ord(s2[i])) for i in range(len(s2))])
+    return "".join([chr(ord(s1[i])^ord(s2[i])) for i in range(len(s2))])
+  return "".join([chr(ord(s1[i])^ord(s2[i])) for i in range(len(s1))])
 
-print xorss("1c0111001f010100061a024b53535009181c".decode('hex'),"686974207468652062756c6c277320657965".decode('hex')).encode('hex')
+txt = xorss("1c0111001f010100061a024b53535009181c".decode('hex'),"686974207468652062756c6c277320657965".decode('hex'))
+print str((txt, txt.encode('hex')))
--- set1/c3.py
+++ set1/c3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import string
-
 def lfet(cc): # letter frequency for english test
   lfe = { "e": 12,"t": 9,"a": 8,"o": 7,"h": 6,"i": 6,"n": 6,"s": 6,"r": 5,"d": 4,"l": 4,"c": 2,"f": 2,"g": 2,"m": 2,"u": 2,"w": 2,"b": 1,"p": 1,"y": 1 }
   count = 0
@@ -12,26 +10,30 @@ def lfet(cc): # letter frequency for english test
       return True
   return False
 
+def cfs(k,s): # character frequency from string
+  chars = dict()
+
+  for cc in s:
+    c = chr(k^ord(cc)).lower()
+
+    if not c in '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c':
+      return None
+
+    chars[c] = chars.get(c,0) + 1
+  return chars
+
+
 def fcxor(ct): # find one character xor
 
-  for x in range(256):
-    mesg = list()
-    chars = dict()
-    lct = len(ct)
+  for key in range(256): # check all possible values
+    chars = cfs(key,ct)
+    if chars == None:
+      continue
 
-    for i in range(lct):
-      c = chr(x^ord(ct[i]))
-      mesg.append(c)
-      chars[c.lower()] = chars.get(c.lower(),0) + 1
-
-      if not c in string.printable:
-        break
-
-    if len(mesg) == lct:
-      for c in chars:
-        chars[c] = round(chars[c]/float(lct),2)
-      if chars.get(' ',0) > 0.1 and lfet(chars):
-        return (x,"".join(mesg))
+    for c in chars:
+      chars[c] = chars[c]/float(len(ct))
+    if chars.get(' ',0) > 0.1 and lfet(chars):
+      return (key, "".join([chr(key^ord(c)) for c in ct]))
 
 if __name__ == '__main__':
   ct = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
