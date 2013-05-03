d0fb9bb0331159ff96994dc2f3f57b5d7c274d4e
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-05-03 09:35:14 +0200

    c18 done, c17 beginning

--- set3/c17.py
+++ set3/c17.py
@@ -1,2 +1,41 @@
 #!/usr/bin/env python
 
+import c10
+import random
+import base64
+
+class cc17:
+
+  def __init__(self):
+    self.key = open("/dev/urandom").read(16)
+
+  def fun1 (self):
+
+    iv = open("/dev/urandom").read(16)
+
+    msgs = "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=\n\
+MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=\n\
+MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==\n\
+MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==\n\
+MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl\n\
+MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==\n\
+MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==\n\
+MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=\n\
+MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=\n\
+MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93"
+
+    return (iv,c10.cbcencrypt(random.choice(msgs.split("\n")),iv,self.key))
+
+  def fun2(self,iv,s):
+
+    try:
+      c10.cbcdecrypt(s,iv,self.key)
+      return True
+    except:
+      return False
+
+if __name__ == "__main__":
+
+  c17 = cc17()
+  a = c17.fun1() # 0 - iv, 1 - encrypted msg
+  print c17.fun2(a[0],a[1])
--- set3/c18.py
+++ set3/c18.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python
 
+from Crypto.Cipher import AES
+import struct
+
+def ctrencrypt(s,nonce,key,blksize=16):
+
+  return ctrdecrypt(s,nonce,key,blksize)
+
+
+def ctrdecrypt(s,nonce,key,blksize=16):
+
+  a = AES.new(key,AES.MODE_ECB)
+  out = ""
+  ctr = 0
+  for i in range(0,len(s),blksize):
+    stream = a.encrypt(struct.pack("<Q",nonce)+struct.pack("<Q",ctr))
+    out += "".join([chr(ord(c1)^ord(c2)) for (c1,c2) in zip(stream,s[i:i+blksize])])
+    ctr += 1
+
+  return out
+
+if __name__ == "__main__":
+  import base64
+
+  txt = base64.b64decode("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==")
+  print ctrdecrypt(txt,0,"YELLOW SUBMARINE")
