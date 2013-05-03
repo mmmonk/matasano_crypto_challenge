0d7cbb964267d5f4401d31b11aed0981a0d75a66
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-05-03 09:39:47 +0200

    c21 started

--- set3/c21.py
+++ set3/c21.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 
+# https://en.wikipedia.org/wiki/Mersenne_twister#Pseudocode
+
+class MT19937:
+
+  def __init__(self,seed):
+    self.mt = list()
+    self.mt.append(seed)
+    for i in range(1,624):
+      self.mt.append()
