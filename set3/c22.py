8fc883ddc5d69b64954d7ccfcdf5c0d56a6ff346
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-05-07 00:35:52 +0200

    c22 cheated

--- set3/c22.py
+++ set3/c22.py
@@ -5,15 +5,20 @@ import time
 import random
 
 #time.sleep(random.randint(40,1000))
-ts = int(time.time())
+ts = int(time.time())-random.randint(40,1000)
 
 rng = c21.MT19937(ts)
 v = rng.extract_number()
 
-v ^=  (v >> 18)
-v ^= ((v << 15) & 0xefc60000)
-v ^= ((v << 7 ) & 0x9d2c5680)
-v ^=  (v >> 11)
-
 print "rng : "+str(v)
 print "seed: "+str(ts)
+
+pseeds = list()
+ts1 = int(time.time())
+for x in range(ts1-3600,ts1):
+  rng2 = c21.MT19937(x)
+  if rng2.extract_number() == v:
+    pseeds.append(x)
+
+print pseeds
+
