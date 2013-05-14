dbc544c3ed4afe75fb52fb63649a1efe2de12a00
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-05-15 00:23:27 +0200

    c19 - done ;)

--- set3/c19.py
+++ set3/c19.py
@@ -2,6 +2,9 @@
 
 import base64
 import c18
+import c3
+import string
+import curses
 
 msgs = "SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==\n\
 Q29taW5nIHdpdGggdml2aWQgZmFjZXM=\n\
@@ -44,7 +47,91 @@ SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=\n\
 VHJhbnNmb3JtZWQgdXR0ZXJseTo=\n\
 QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4="
 
+# encrypting above messages
 key = open("/dev/urandom").read(16)
-
+cts = list()
+mxlen = 0    # max length of the CT
+milen = 999  # min length of the CT
 for msg in msgs.split("\n"):
-  print c18.ctrencrypt(base64.b64decode(msg),0,key).encode('hex')
+  cts.append(c18.ctrencrypt(base64.b64decode(msg),0,key))
+  l = len(cts[-1])
+  if l > mxlen:
+    mxlen = l
+  if l < milen:
+    milen = l
+
+
+# breaking it
+
+# prepare the key
+key = [0]*mxlen
+
+# do some automated guesses
+rev = range(milen)
+for i in range(milen):
+  rev[i] = list()
+
+for i in range(len(cts)):
+  ct = cts[i]
+  for j in range(milen):
+    rev[j].append(ct[j])
+
+for i in range(milen):
+  out = c3.fcxor(rev[i])
+  if out != None:
+    key[i] = out[0]
+
+### init ncurses and do manual guessing
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(1)
+curses.curs_set(0)
+x = 0
+y = 0
+try:
+  while True:
+    stdscr.addstr(0,0,"("+str(x)+","+str(y)+") - press ctrl+c to exit\n\n")
+    for ct in cts:
+      out = "".join([ chr(ord(c1)^c2) for (c1,c2) in zip(ct,key)]).encode('string_escape')
+      stdscr.addstr(out+"\n")
+
+    # this prints the current value for the key in hex format
+    pkey = "".join([ chr(k1) for k1 in key])
+    stdscr.addstr("\nkey: "+str(pkey).encode('hex')+"\n")
+
+    # this highlights the current character
+    stdscr.addstr(y+2,x,chr(stdscr.inch(y+2,x) & 0xff),curses.A_REVERSE)
+
+    # refresh the screen
+    stdscr.refresh()
+
+    # wait for input
+    c = stdscr.getch()
+
+    # normal moving around the screen
+    if c == curses.KEY_LEFT:
+      x = (x - 1) % len(key)
+    elif c == curses.KEY_RIGHT:
+      x = (x + 1) % len(key)
+    elif c == curses.KEY_UP:
+      y = (y - 1) % len(cts)
+    elif c == curses.KEY_DOWN:
+      y = (y + 1) % len(cts)
+
+    # modifiying the key
+    else:
+      try:
+        key[x] = ord(cts[y][x])^c
+      except:
+        pass
+
+except KeyboardInterrupt:
+  pass
+
+### cleanup terminal after ncurses
+curses.curs_set(1)
+curses.nocbreak()
+stdscr.keypad(0)
+curses.echo()
+curses.endwin()
