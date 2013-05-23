bdea94e6b0229d8e5161fa600032d2509b0ff19f
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-05-23 17:44:00 +0200

    MD4 - almost done

--- set4/c30.py
+++ set4/c30.py
@@ -19,6 +19,26 @@ def padding(msglen):
 
   return pad
 
+
+def f(x,y,z):
+  return ((x&y) | ((~x)&z))
+
+def g(x,y,z):
+  return ((x&y) | (x&z) | (y&z))
+
+def h(x.y.z):
+  return (x ^ y ^ z)
+
+def ff(a, b, c, d, x, s):
+  return (a + f(b,c,d) + x ) << s
+
+def gg(a, b, c, d, x, s):
+  return (a + g(b,c,d) + x + 0x5A827999) << s
+
+def hh(a, b, c, d, x, s):
+  return (a + h(b,c,d) + x + 0x6ED9EBA1) << s
+
+
 class md4:
   '''
   https://tools.ietf.org/html/rfc1320
@@ -52,50 +72,84 @@ class md4:
     self.C = hs[2]
     self.D = hs[3]
 
-  def digest(self,msg=""):
+  def digest(self,imsg=""):
 
-    if msg == "":
-      msg = self.msg
+    msg = self.msg
+    if imsg != "":
+      msg = imsg
 
     msg += padding(len(msg))
 
     nchunk = 0
     for i in xrange(0,int(len(msg)/64)):
-      chunk = msg[nchunk*64:(nchunk+1)*64]
-      nchunk += 1
-      w = list(struct.unpack('>IIIIIIIIIIIIIIII',chunk))
-      for j in xrange(16,80):
-        w.append(rol32(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16],1))
+      X = list(struct.unpack('>IIIIIIIIIIIIIIII',msg[nchunk*64:(nchunk+1)*64]))
 
-      AA = self.A
-      BB = self.B
-      CC = self.C
-      DD = self.D
+      a = self.A
+      b = self.B
+      c = self.C
+      d = self.D
 
-      for j in xrange(0,80):
-        if j < 20:
-          f = (b & c) | ((~ b) & d)
-        elif j < 40:
-          g = b ^ c ^ d
-        elif j < 60:
-          h = (b & c) | (b & d) | (c & d)
+      FF (a, b, c, d, x[ 0], S11)
+      FF (d, a, b, c, x[ 1], S12)
+      FF (c, d, a, b, x[ 2], S13)
+      FF (b, c, d, a, x[ 3], S14)
+      FF (a, b, c, d, x[ 4], S11)
+      FF (d, a, b, c, x[ 5], S12)
+      FF (c, d, a, b, x[ 6], S13)
+      FF (b, c, d, a, x[ 7], S14)
+      FF (a, b, c, d, x[ 8], S11)
+      FF (d, a, b, c, x[ 9], S12)
+      FF (c, d, a, b, x[10], S13)
+      FF (b, c, d, a, x[11], S14)
+      FF (a, b, c, d, x[12], S11)
+      FF (d, a, b, c, x[13], S12)
+      FF (c, d, a, b, x[14], S13)
+      FF (b, c, d, a, x[15], S14)
 
-        temp = (rol32(a,5) + f + e + k + w[j]) & 0xffffffff
-        e = d
-        d = c
-        c = rol32(b,30)
-        b = a
-        a = temp
+      GG (a, b, c, d, x[ 0], S21)
+      GG (d, a, b, c, x[ 4], S22)
+      GG (c, d, a, b, x[ 8], S23)
+      GG (b, c, d, a, x[12], S24)
+      GG (a, b, c, d, x[ 1], S21)
+      GG (d, a, b, c, x[ 5], S22)
+      GG (c, d, a, b, x[ 9], S23)
+      GG (b, c, d, a, x[13], S24)
+      GG (a, b, c, d, x[ 2], S21)
+      GG (d, a, b, c, x[ 6], S22)
+      GG (c, d, a, b, x[10], S23)
+      GG (b, c, d, a, x[14], S24)
+      GG (a, b, c, d, x[ 3], S21)
+      GG (d, a, b, c, x[ 7], S22)
+      GG (c, d, a, b, x[11], S23)
+      GG (b, c, d, a, x[15], S24)
 
-      self.A = (self.h0 + a) & 0xffffffff
-      self.B = (self.h1 + b) & 0xffffffff
-      self.C = (self.h2 + c) & 0xffffffff
-      self.D = (self.h3 + d) & 0xffffffff
+      HH (a, b, c, d, x[ 0], S31)
+      HH (d, a, b, c, x[ 8], S32)
+      HH (c, d, a, b, x[ 4], S33)
+      HH (b, c, d, a, x[12], S34)
+      HH (a, b, c, d, x[ 2], S31)
+      HH (d, a, b, c, x[10], S32)
+      HH (c, d, a, b, x[ 6], S33)
+      HH (b, c, d, a, x[14], S34)
+      HH (a, b, c, d, x[ 1], S31)
+      HH (d, a, b, c, x[ 9], S32)
+      HH (c, d, a, b, x[ 5], S33)
+      HH (b, c, d, a, x[13], S34)
+      HH (a, b, c, d, x[ 3], S31)
+      HH (d, a, b, c, x[11], S32)
+      HH (c, d, a, b, x[ 7], S33)
+      HH (b, c, d, a, x[15], S34)
+
+
+      self.A = (self.A + a) & 0xffffffff
+      self.B = (self.B + b) & 0xffffffff
+      self.C = (self.C + c) & 0xffffffff
+      self.D = (self.D + d) & 0xffffffff
 
     return struct.pack('>IIIII',self.h0,self.h1,self.h2,self.h3,self.h4)
 
-  def hexdigest(self):
-    return self.digest().encode('hex')
+  def hexdigest(self,imsg=""):
+    return self.digest(imsg).encode('hex')
 
 if __name__ == "__main__":
   try:
