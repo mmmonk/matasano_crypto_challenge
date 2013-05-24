e0b9ad620a943da1b0d5ffa8e2e5157d50779659
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-05-24 16:33:47 +0200

    c28 and c30 - more cleanup MD4 still doesn't work

--- set4/c28.py
+++ set4/c28.py
@@ -29,17 +29,55 @@ class sha1:
 
   def __init__(self,imsg=""):
 
-    self._setinit()
+    self.__setinit()
     self.mesg = imsg
     self.omsg = ""
     self.omlen = 0
 
-  def _setinit(self):
+  def __setinit(self):
     self.h0 = 0x67452301
     self.h1 = 0xEFCDAB89
     self.h2 = 0x98BADCFE
     self.h3 = 0x10325476
     self.h4 = 0xC3D2E1F0
+
+  def __transform(self,w):
+
+    for j in range(16,80):
+      w.append(rol32(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16],1))
+
+    a = self.h0
+    b = self.h1
+    c = self.h2
+    d = self.h3
+    e = self.h4
+
+    for j in range(0,80):
+      if j < 20:
+        f = (b & c) | ((~ b) & d)
+        k = 0x5A827999
+      elif j < 40:
+        f = b ^ c ^ d
+        k = 0x6ED9EBA1
+      elif j < 60:
+        f = (b & c) | (b & d) | (c & d)
+        k = 0x8F1BBCDC
+      else:
+        f = b ^ c ^ d
+        k = 0xCA62C1D6
+
+      temp = (rol32(a,5) + f + e + k + w[j]) & 0xFFFFFFFF
+      e = d
+      d = c
+      c = rol32(b,30)
+      b = a
+      a = temp
+
+    self.h0 = (self.h0 + a) & 0xFFFFFFFF
+    self.h1 = (self.h1 + b) & 0xFFFFFFFF
+    self.h2 = (self.h2 + c) & 0xFFFFFFFF
+    self.h3 = (self.h3 + d) & 0xFFFFFFFF
+    self.h4 = (self.h4 + e) & 0xFFFFFFFF
 
   def extlen(self,orghash,msglen):
 
@@ -62,46 +100,11 @@ class sha1:
 
     msg += padding(self.omlen+len(msg))
 
-    for i in xrange(0,len(msg)/64):
-      w = list(struct.unpack('>IIIIIIIIIIIIIIII',msg[i*64:(i+1)*64]))
-      for j in xrange(16,80):
-        w.append(rol32(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16],1))
-
-      a = self.h0
-      b = self.h1
-      c = self.h2
-      d = self.h3
-      e = self.h4
-
-      for j in xrange(0,80):
-        if j < 20:
-          f = (b & c) | ((~ b) & d)
-          k = 0x5A827999
-        elif j < 40:
-          f = b ^ c ^ d
-          k = 0x6ED9EBA1
-        elif j < 60:
-          f = (b & c) | (b & d) | (c & d)
-          k = 0x8F1BBCDC
-        else:
-          f = b ^ c ^ d
-          k = 0xCA62C1D6
-
-        temp = (rol32(a,5) + f + e + k + w[j]) & 0xFFFFFFFF
-        e = d
-        d = c
-        c = rol32(b,30)
-        b = a
-        a = temp
-
-      self.h0 = (self.h0 + a) & 0xFFFFFFFF
-      self.h1 = (self.h1 + b) & 0xFFFFFFFF
-      self.h2 = (self.h2 + c) & 0xFFFFFFFF
-      self.h3 = (self.h3 + d) & 0xFFFFFFFF
-      self.h4 = (self.h4 + e) & 0xFFFFFFFF
+    for i in range(0,len(msg)/64):
+      self.__transform(list(struct.unpack('>IIIIIIIIIIIIIIII',msg[i*64:(i+1)*64])))
 
     out = struct.pack('>IIIII',self.h0,self.h1,self.h2,self.h3,self.h4)
-    self._setinit()
+    self.__setinit()
     return out
 
   def hexdigest(self,imsg=""):
--- set4/c30.py
+++ set4/c30.py
@@ -6,23 +6,17 @@ import struct
 def rol32(word,count):
     return (word << count | word >> (32 - count)) & 0xFFFFFFFF
 
-def f(x,y,z):
-  return ((x&y) | ((~x)&z))
+def r1(a, b, c, d, x, s):
+  f = ((b&c) | ((~b)&d))
+  return rol32((a + f + x ) & 0xFFFFFFFF,s)
 
-def g(x,y,z):
-  return ((x&y) | (x&z) | (y&z))
+def r2(a, b, c, d, x, s):
+  f = ((b&c) | (b&d) | (c&d))
+  return rol32((a + f + x + 0x5A827999) & 0xFFFFFFFF,s)
 
-def h(x,y,z):
-  return (x ^ y ^ z)
-
-def FF(a, b, c, d, x, s):
-  return rol32((a + f(b,c,d) + x ) & 0xFFFFFFFF,s)
-
-def GG(a, b, c, d, x, s):
-  return rol32((a + g(b,c,d) + x + 0x5A827999) & 0xFFFFFFFF,s)
-
-def HH(a, b, c, d, x, s):
-  return rol32((a + h(b,c,d) + x + 0x6ED9EBA1) & 0xFFFFFFFF,s)
+def r3(a, b, c, d, x, s):
+  f = b ^ c ^ d
+  return rol32((a + f + x + 0x6ED9EBA1) & 0xFFFFFFFF,s)
 
 def padding(msglen):
 
@@ -46,10 +40,10 @@ class md4:
 
   def __init__(self,imsg=""):
 
-    self._setinit()
+    self.__setinit()
     self.msg = imsg
 
-  def _setinit(self):
+  def __setinit(self):
 
     self.A = 0x67452301
     self.B = 0xEFCDAB89
@@ -65,6 +59,69 @@ class md4:
     self.C = hs[2]
     self.D = hs[3]
 
+  def __transform(self,x):
+
+    a = self.A
+    b = self.B
+    c = self.C
+    d = self.D
+
+    a = r1(a, b, c, d, x[ 0], 3)
+    d = r1(d, a, b, c, x[ 1], 7)
+    c = r1(c, d, a, b, x[ 2], 11)
+    b = r1(b, c, d, a, x[ 3], 19)
+    a = r1(a, b, c, d, x[ 4], 3)
+    d = r1(d, a, b, c, x[ 5], 7)
+    c = r1(c, d, a, b, x[ 6], 11)
+    b = r1(b, c, d, a, x[ 7], 19)
+    a = r1(a, b, c, d, x[ 8], 3)
+    d = r1(d, a, b, c, x[ 9], 7)
+    c = r1(c, d, a, b, x[10], 11)
+    b = r1(b, c, d, a, x[11], 19)
+    a = r1(a, b, c, d, x[12], 3)
+    d = r1(d, a, b, c, x[13], 7)
+    c = r1(c, d, a, b, x[14], 11)
+    b = r1(b, c, d, a, x[15], 19)
+
+    a = r2(a, b, c, d, x[ 0], 3)
+    d = r2(d, a, b, c, x[ 4], 5)
+    c = r2(c, d, a, b, x[ 8], 9)
+    b = r2(b, c, d, a, x[12], 13)
+    a = r2(a, b, c, d, x[ 1], 3)
+    d = r2(d, a, b, c, x[ 5], 5)
+    c = r2(c, d, a, b, x[ 9], 9)
+    b = r2(b, c, d, a, x[13], 13)
+    a = r2(a, b, c, d, x[ 2], 3)
+    d = r2(d, a, b, c, x[ 6], 5)
+    c = r2(c, d, a, b, x[10], 9)
+    b = r2(b, c, d, a, x[14], 13)
+    a = r2(a, b, c, d, x[ 3], 3)
+    d = r2(d, a, b, c, x[ 7], 5)
+    c = r2(c, d, a, b, x[11], 9)
+    b = r2(b, c, d, a, x[15], 13)
+
+    a = r3(a, b, c, d, x[ 0], 3)
+    d = r3(d, a, b, c, x[ 8], 9)
+    c = r3(c, d, a, b, x[ 4], 11)
+    b = r3(b, c, d, a, x[12], 15)
+    a = r3(a, b, c, d, x[ 2], 3)
+    d = r3(d, a, b, c, x[10], 9)
+    c = r3(c, d, a, b, x[ 6], 11)
+    b = r3(b, c, d, a, x[14], 15)
+    a = r3(a, b, c, d, x[ 1], 3)
+    d = r3(d, a, b, c, x[ 9], 9)
+    c = r3(c, d, a, b, x[ 5], 11)
+    b = r3(b, c, d, a, x[13], 15)
+    a = r3(a, b, c, d, x[ 3], 3)
+    d = r3(d, a, b, c, x[11], 9)
+    c = r3(c, d, a, b, x[ 7], 11)
+    b = r3(b, c, d, a, x[15], 15)
+
+    self.A = (self.A + a) & 0xFFFFFFFF
+    self.B = (self.B + b) & 0xFFFFFFFF
+    self.C = (self.C + c) & 0xFFFFFFFF
+    self.D = (self.D + d) & 0xFFFFFFFF
+
   def digest(self,imsg=""):
 
     msg = self.msg
@@ -73,74 +130,11 @@ class md4:
 
     msg += padding(len(msg))
 
-    for i in xrange(0,len(msg)/64):
-      X = list(struct.unpack('>IIIIIIIIIIIIIIII',msg[i*64:(i+1)*64]))
-
-      print X
-
-      a = self.A
-      b = self.B
-      c = self.C
-      d = self.D
-
-      a = FF(a, b, c, d, X[ 0], 3)
-      d = FF(d, a, b, c, X[ 1], 7)
-      c = FF(c, d, a, b, X[ 2], 11)
-      b = FF(b, c, d, a, X[ 3], 19)
-      a = FF(a, b, c, d, X[ 4], 3)
-      d = FF(d, a, b, c, X[ 5], 7)
-      c = FF(c, d, a, b, X[ 6], 11)
-      b = FF(b, c, d, a, X[ 7], 19)
-      a = FF(a, b, c, d, X[ 8], 3)
-      d = FF(d, a, b, c, X[ 9], 7)
-      c = FF(c, d, a, b, X[10], 11)
-      b = FF(b, c, d, a, X[11], 19)
-      a = FF(a, b, c, d, X[12], 3)
-      d = FF(d, a, b, c, X[13], 7)
-      c = FF(c, d, a, b, X[14], 11)
-      b = FF(b, c, d, a, X[15], 19)
-
-      a = GG(a, b, c, d, X[ 0], 3)
-      d = GG(d, a, b, c, X[ 4], 5)
-      c = GG(c, d, a, b, X[ 8], 9)
-      b = GG(b, c, d, a, X[12], 13)
-      a = GG(a, b, c, d, X[ 1], 3)
-      d = GG(d, a, b, c, X[ 5], 5)
-      c = GG(c, d, a, b, X[ 9], 9)
-      b = GG(b, c, d, a, X[13], 13)
-      a = GG(a, b, c, d, X[ 2], 3)
-      d = GG(d, a, b, c, X[ 6], 5)
-      c = GG(c, d, a, b, X[10], 9)
-      b = GG(b, c, d, a, X[14], 13)
-      a = GG(a, b, c, d, X[ 3], 3)
-      d = GG(d, a, b, c, X[ 7], 5)
-      c = GG(c, d, a, b, X[11], 9)
-      b = GG(b, c, d, a, X[15], 13)
-
-      a = HH(a, b, c, d, X[ 0], 3)
-      d = HH(d, a, b, c, X[ 8], 9)
-      c = HH(c, d, a, b, X[ 4], 11)
-      b = HH(b, c, d, a, X[12], 15)
-      a = HH(a, b, c, d, X[ 2], 3)
-      d = HH(d, a, b, c, X[10], 9)
-      c = HH(c, d, a, b, X[ 6], 11)
-      b = HH(b, c, d, a, X[14], 15)
-      a = HH(a, b, c, d, X[ 1], 3)
-      d = HH(d, a, b, c, X[ 9], 9)
-      c = HH(c, d, a, b, X[ 5], 11)
-      b = HH(b, c, d, a, X[13], 15)
-      a = HH(a, b, c, d, X[ 3], 3)
-      d = HH(d, a, b, c, X[11], 9)
-      c = HH(c, d, a, b, X[ 7], 11)
-      b = HH(b, c, d, a, X[15], 15)
-
-      self.A = (self.A + a) & 0xFFFFFFFF
-      self.B = (self.B + b) & 0xFFFFFFFF
-      self.C = (self.C + c) & 0xFFFFFFFF
-      self.D = (self.D + d) & 0xFFFFFFFF
+    for i in range(0,len(msg)/64):
+      self.__transform(list(struct.unpack('>IIIIIIIIIIIIIIII',msg[i*64:(i+1)*64])))
 
     out = struct.pack('>IIII',self.A,self.B,self.C,self.D)
-    self._setinit()
+    self.__setinit()
     return out
 
   def hexdigest(self,imsg=""):
@@ -172,9 +166,9 @@ if __name__ == "__main__":
   except:
     msg = "some test text message"
 
-  #key = open("/dev/urandom").read(8).encode('hex')
-  #print md4(key+msg).hexdigest()+" "+key+msg
   if not md4().test():
     print "tests failed"
   else:
     print "all tests OK"
+    key = open("/dev/urandom").read(8).encode('hex')
+    print md4(key+msg).hexdigest()+" "+key+msg
