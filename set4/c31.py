0e56a88254e6629a7f77c0ebfd3e0bcd27b49db8
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-05-26 00:53:52 +0200

    c31 - server done

--- set4/c31.py
+++ set4/c31.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-import BaseHTTPServer
+import web
 import c28
+import time
 
 def hmac(key,msg,algo):
   '''
@@ -18,14 +19,44 @@ def hmac(key,msg,algo):
 
   return algo(opad + algo(ipad + msg).digest())
 
-def run(server_class=BaseHTTPServer.HTTPServer, handler_class=BaseHTTPServer.BaseHTTPRequestHandler):
-  server_address = ('', 8000)
-  httpd = server_class(server_address, handler_class)
-  httpd.serve_forever()
+
+class hmac_check:
+  def GET(self, name):
+    var = web.input(file="c31.py",signature="0")
+    try:
+      sig = hmac("secret",open(var.file).read(),c28.sha1).digest()
+    except:
+      sig = "\x00"
+    if insecure_compare(sig,var.signature):
+      return 'OK File: ' + str(var.file) + ' ' + var.signature
+
+      #raise web.internalerror()
+
+    return 'Nope File: ' + str(var.file) + ' ' + sig.encode('hex')
+
+def insecure_compare(s1,hs2):
+  try:
+    s2 = hs2.decode('hex')
+  except:
+    return False
+
+  for (c1,c2) in zip (s1,s2):
+    if c1 != c2:
+      return False
+    time.sleep(0.5)
+  return True
+
+class MyApplication(web.application):
+  def run(self, port=9000, *middleware):
+    func = self.wsgifunc(*middleware)
+    return web.httpserver.runsimple(func, ('127.0.0.1', port))
+
+def internalerror():
+      return web.internalerror("you are wrrooooooonggg.")
 
 if __name__ == "__main__":
 
-  print hmac("key","The quick brown fox jumps over the lazy dog",c28.sha1).hexdigest()
-  print hmac("","",c28.sha1).hexdigest()
-  run()
-
+  urls = ( '/test?(.*)', 'hmac_check')
+  app = MyApplication(urls, globals())
+  app.internalerror = internalerror
+  app.run()
