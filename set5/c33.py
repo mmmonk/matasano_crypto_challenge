6370be28c567d9a750dcdf7336c430b161c6c271
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-05-31 17:08:23 +0200

    c33 - small changes

--- set5/c33.py
+++ set5/c33.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
   import random
 
   p = 0xffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff
-  g = 2
+  g = 2**16+1
 
   Alice = dh(random.randint(0,100000),p,g)
   Bob = dh(random.randint(0,100000),p,g)
@@ -43,5 +43,5 @@ if __name__ == "__main__":
     print "OK"
   else:
     print "Something fucked up"
-  print Bob_secret
-  print Alice_secret
+  print hex(Bob_secret)
+  print hex(Alice_secret)
