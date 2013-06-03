5129ebb75398ce04dccc2bdcd3bbcad32c98c6d3
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-06-03 17:19:43 +0200

    c39 - RSA added basic links

---
+++ set5/c39.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import Crypto.Util.number
+
+# https://en.wikipedia.org/wiki/Modular_multiplicative_inverse
+# https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
+# https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+# https://en.wikipedia.org/wiki/RSA_%28algorithm%29
+# Crypto.Util.number.getStrongPrime
