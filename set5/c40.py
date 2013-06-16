7c8624d35d84f7ec5206b9705ff8026789264437
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-06-16 08:46:47 +0200

    c40 - beginning

---
+++ set5/c40.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from c39 import RSA # rsa
+
+if __name__ == "__main__":
+  cleartext  = "this is just a test of a message that will be encrypted using RSA private/public key cryptography"
+  pub1,priv1 = RSA().keygen(l=2048,s=False) # s=False gives e=3
+  ct1 = RSA().encrypt(cleartext,pub1)
+  pub2,priv2 = RSA().keygen(l=2048,s=False) # s=False gives e=3
+  ct2 = RSA().encrypt(cleartext,pub1)
+  pub3,priv3 = RSA().keygen(l=2048,s=False) # s=False gives e=3
+  ct3 = RSA().encrypt(cleartext,pub1)
+
