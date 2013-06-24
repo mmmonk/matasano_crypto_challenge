12a6e102266cd3b3edcceece7a2dd58b3c3a3ad3
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-06-24 23:12:05 +0200

    c40 - done

--- set5/c40.py
+++ set5/c40.py
@@ -5,12 +5,12 @@ from c36 import i2s, s2i
 
 if __name__ == "__main__":
 
-  cleartext  = "abc"
-  pub0,priv0 = RSA().keygen(l=256,s=False) # s=False gives e=3
+  cleartext  = "attack at dawn"
+  pub0,priv0 = RSA().keygen(l=2048,s=False) # s=False gives e=3
   ct0 = RSA().encrypt(cleartext,pub0)
-  pub1,priv1 = RSA().keygen(l=256,s=False) # s=False gives e=3
+  pub1,priv1 = RSA().keygen(l=2048,s=False) # s=False gives e=3
   ct1 = RSA().encrypt(cleartext,pub1)
-  pub2,priv2 = RSA().keygen(l=256,s=False) # s=False gives e=3
+  pub2,priv2 = RSA().keygen(l=2048,s=False) # s=False gives e=3
   ct2 = RSA().encrypt(cleartext,pub2)
 
   assert RSA().decrypt(ct0,priv0) == cleartext, "error on key0"
@@ -33,9 +33,6 @@ if __name__ == "__main__":
   result  = (c_0 * m_s_0 * invmod(m_s_0, n_0))
   result += (c_1 * m_s_1 * invmod(m_s_1, n_1))
   result += (c_2 * m_s_2 * invmod(m_s_2, n_2))
+  result %= (n_0 * n_1 * n_2)
 
-  #result = pow(Decimal(result),Decimal(1/3.0))
-
-  print s2i("abc")
-  print s2i("abc")**3
-  print int(result)
+  print i2s(int(round(pow(result,1/3.0),0)))
