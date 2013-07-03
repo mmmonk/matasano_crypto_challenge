66698c5a0c2b7c6587e4471e1cfdd22c07e739e8
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-07-03 17:39:23 +0200

    c41 - looks nicer

--- set6/c41.py
+++ set6/c41.py
@@ -5,17 +5,19 @@ from c36 import i2s, s2i
 
 if __name__ == "__main__":
 
-  msg = "attack after breakfest"
+  msg = "attack after the breakfast"
 
   rsa = RSA()
   pub,priv = rsa.keygen()
   C = rsa.encrypt(msg,pub)
-  assert rsa.decrypt(C,priv) == msg, "bug in my RSA"
+  assert rsa.decrypt(C,priv) == msg, "bug in my RSA, decryption didn't provide the same clear text"
 
-  S = 3
-  C1 = list()
-  C1.append(i2s((pow(S,pub[0],pub[1])*s2i(C[0])) % pub[1]))
-  if C1[0] != C[0]:
+  S = 3       # lowest possible S
+  C1 = list() # the cipher text is a list
+  for ct in C: # lets iterate over the ciphertext
+    C1.append(i2s((pow(S,pub[0],pub[1])*s2i(ct)) % pub[1])) #
+
+  if C1 != C: # the C1 has to be different the C
     P1 = rsa.decrypt(C1,priv)
     print i2s((s2i(P1)/S)%pub[1])
   else:
