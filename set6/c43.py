33f2b8e38bbd8ce3370526bd1f93b4c10bc893ca
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-07-08 22:37:05 +0200

    c43 - started work on DSA

--- set6/c43.py
+++ set6/c43.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 
 # https://en.wikipedia.org/wiki/Digital_Signature_Algorithm
+
+import Crypto.Util.number
+
+class DSA:
+
+  def __init__(self,L = 3072, N = 256):
+    q = Crypto.Util.number.getPrime(N)
+    p =
+    g =
+
+
