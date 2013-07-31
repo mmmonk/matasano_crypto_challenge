e8a5d7de9a38803acc70cca9da9cdcc69495f838
Marek Lukaszuk <m.lukaszuk@gmail.com>
2013-08-01 00:14:27 +0200

    c43 - added initial values

--- set6/c43.py
+++ set6/c43.py
@@ -3,12 +3,14 @@
 # https://en.wikipedia.org/wiki/Digital_Signature_Algorithm
 
 import Crypto.Util.number
+import hashlib
 
 class DSA:
 
-  def __init__(self,L = 3072, N = 256):
-    q = Crypto.Util.number.getPrime(N)
-    p =
-    g =
+  def __init__(self):
+    self.p = 800000000000000089e1855218a0e7dac38136ffafa72eda7859f2171e25e65eac698c1702578b07dc2a1076da241c76c62d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebeac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc871a584471bb1
+    self.q = f4f47f05794b256174bba6e9b396a7707e563c5b
+    self.g = 5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119458fef538b8fa4046c8db53039db620c094c9fa077ef389b5322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a0470f5b64c36b625a097f1651fe775323556fe00b3608c887892878480e99041be601a62166ca6894bdd41a7054ec89f756ba9fc95302291
 
-
+  def sign(self,m):
+    k =
